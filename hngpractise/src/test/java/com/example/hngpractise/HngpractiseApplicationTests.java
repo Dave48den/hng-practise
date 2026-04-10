2d369832a6d4aac4fa1e31b0d37bcd430d491f44
King David <chukwukakingdavid5@gmail.com>
2026-04-10 08:35:58 +0000

    Initial Spring Boot project with HelloController

---
+++ hngpractise/src/test/java/com/example/hngpractise/HngpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hngpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngpractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/example/hngpractise/HngpractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.hngpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HngpractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HngpractiseApplication.class, args);
+	}
+
+}
